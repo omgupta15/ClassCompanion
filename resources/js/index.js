e4c588fd1ae1e6378920184ce9fdffe5db1ab75e
Om Gupta <om.gupta.1507@gmail.com>
2020-11-27 11:09:31 +0530

    Added alert for mobile users.

--- resources/js/index.js
+++ resources/js/index.js
@@ -375,3 +375,14 @@ window.onresize = windowResized;
 $('[data-toggle="tooltip"]').tooltip();
 reloadNotes();
 windowResized();
+
+window.onload = function() {
+    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+    if (mobile) {
+        Swal.fire(
+            "Desktop Recommended",
+            "This website works best when used on a desktop computer.",
+            "info"
+        );              
+    }
+};
